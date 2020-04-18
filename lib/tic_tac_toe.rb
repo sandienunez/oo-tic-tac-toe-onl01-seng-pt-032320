ff9717afbdc86cc8c8e8ad457ad76f3614fcf7da
Sandie Nunez <snunez@vt.edu>
2020-04-18 20:07:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,7 +77,86 @@ end
   end 
 
 def won?
-end 
+WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
+      return win_combination
+    else
+      # do nothing
+    end
+  end
+  return nil
+end
+
+def full?(array)
+  counter = 0
+  array.each do |element|
+    if (element == "X" || element == "O")
+      counter += 1
+    else
+      # do nothing
+    end
+  end
+  if counter == 9
+    return true
+  else
+    return false
+  end
+end
+
+def draw?(array)
+  if full?(array) && !won?(array)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(array)
+  if ( won?(array) || draw?(array) || full?(array) )
+    return true
+  else
+    return false
+  end
+end
+
+def winner(array)
+  if won?(array)
+    WIN_COMBINATIONS.each do |win_combination|
+      position_1 = array[win_combination[0]]
+      position_2 = array[win_combination[1]]
+      position_3 = array[win_combination[2]]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        return "X"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return "O"
+      else
+        # do nothing
+      end
+    end
+  else
+    return nil
+  end
+end
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations " + winner(board) + "!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  else
+    #do nothing
+  end
+end
+
 
 end 
 
