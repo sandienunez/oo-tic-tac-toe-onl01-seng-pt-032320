210591536e85953454ad4f541b949b42c0de77d0
Sandie Nunez <snunez@vt.edu>
2020-04-18 19:43:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,42 +18,9 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input.to_i - 1
-end
 
-def move(board, index, current_player)
-  board[index] = current_player
-end
 
-def position_taken?(array, index)
-  if (array[index] == " " || array[index] == "" || array[index] == nil)
-    return false
-  else
-    return true
-  end
-end
 
-def valid_move?(array, index)
-  if (index < 0 || index > 8)
-    return false
-  elsif position_taken?(array, index)
-    return false
-  else
-    return true
-  end
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
-end
 
 def turn_count(board)
   counter = 0 
@@ -65,7 +32,10 @@ end
 counter 
 end 
 
-
+def current_player(baord)
+  turn_count(board) % 2 == 0 ? "X":"O"
+end 
+end 
 
 
 # #def valid_move?(position)
