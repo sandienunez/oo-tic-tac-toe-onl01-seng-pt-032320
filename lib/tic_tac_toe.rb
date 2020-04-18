10c085859f064574f4b0efb560c78d9a0ec0eaaa
Sandie Nunez <snunez@vt.edu>
2020-04-18 20:27:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -164,10 +164,23 @@ def over?
   end
 end
 
-def winner
-   if won?(@board)
-      return board[won?(@board)[0]]
-   end
+def winner(array)
+  if won?(array)
+    WIN_COMBINATIONS.each do |win_combination|
+      position_1 = array[win_combination[0]]
+      position_2 = array[win_combination[1]]
+      position_3 = array[win_combination[2]]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        return "X"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return "O"
+      else
+        # do nothing
+      end
+    end
+  else
+    return nil
+  end
 end
 
 def play(board)
