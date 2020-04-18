e272bf72346c4e9cff7034c075c2af560f4643c3
Sandie Nunez <snunez@vt.edu>
2020-04-18 21:39:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -185,8 +185,6 @@ def play
 end
 end 
 
-@board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-play
 
 # #def valid_move?(position)
 #   position.between?(0, 8) && position_taken?(position) == false #can put bang in front of !position_taken?(position)
