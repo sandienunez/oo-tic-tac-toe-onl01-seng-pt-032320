9d52fd9f81d4eac47344487259a0dc7f055263db
Sandie Nunez <snunez@vt.edu>
2020-04-18 19:13:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,20 +1,22 @@
-def valid_move?(position)
-  position.between?(0, 8) && position_taken?(position) == false #can put bang in front of !position_taken?(position)
-end 
 
-def turn
-  input = gets.strip
-  converted_input = input_to_index(input)
-  if valid_move?(converted_input)
-    token = current_player
-    move(converted_input)
-    display_board
-  else
-    turn 
-  end 
+
+# #def valid_move?(position)
+#   position.between?(0, 8) && position_taken?(position) == false #can put bang in front of !position_taken?(position)
+# end 
+
+# def turn
+#   input = gets.strip
+#   converted_input = input_to_index(input)
+#   if valid_move?(converted_input)
+#     token = current_player
+#     move(converted_input)
+#     display_board
+#   else
+#     turn 
+#   end 
   
-end 
+# end 
 
-def turn_count
-  @board.count{|token| token == "X" \\ token == "0"'
-end 
+# def turn_count
+#   @board.count{|token| token == "X" \\ token == "0"'
+# end 
